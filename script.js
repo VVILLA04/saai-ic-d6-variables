c35a0a9a671a30afe6dede3c27b5941dbc64741c
VVILLA04 <vanvilla14@gmail.com>
2024-11-12 00:48:01 +0000

    javascript 101

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Let's Fucking Goooo")
+console.log("I love Heather")
+let animal="panda"
+console.log(animal)
+
+let favoriteFood="Beans"
+console.log(favoriteFood)
+
+favoriteFood="Burgers"
+console.log(favoriteFood)
+
+const favoriteHoliday="Christmas"
+console.log(favoriteHoliday)
+
+console.log("favoriteFood", favoriteFood)
+console.log("favoriteHoliday", favoriteHoliday)
